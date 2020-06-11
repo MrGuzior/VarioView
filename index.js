926b90dee6ba44ce8f7efc5a8033530746df137c
MrGuzior <konrad.guzior@gmail.com>
2020-06-11 17:58:50 +0200

    Added nodemon as dev dependency

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const express = require('express');
 const IGCparser = require('igc-parser');
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to VarioView');
+})
+
+app.listen(3000, () => console.log('VarioView listening on port 3000!'))
